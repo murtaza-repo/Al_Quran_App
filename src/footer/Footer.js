535e34e38394f0d8b4c27d560be3fdbe46fa16f0
Saiftechie <murtazakhachrod@gmail.com>
2022-06-02 11:43:21 +0530

    Vesrion 1.1

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div
         className="text-center p-3"
       >
-        © 2022 Developed with ❤️ by
+        © 2022 Made with ❤️ by
         <a className="text-decoration-none" rel="noreferrer" target="_blank" href="https://murtaza-k.web.app/">
           &nbsp;Murtaza Khachrodwala
         </a>
